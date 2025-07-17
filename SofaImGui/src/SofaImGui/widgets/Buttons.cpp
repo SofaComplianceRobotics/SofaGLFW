22dc9ffbf9f0cb18df61ddd974cdcec1ffa6bd85
EulalieCoevoet <eulalie.coevoet@gmail.com>
2025-07-17 13:01:54 +0200

    fixes modifier block item size

--- SofaImGui/src/SofaImGui/widgets/Buttons.cpp
+++ SofaImGui/src/SofaImGui/widgets/Buttons.cpp
@@ -204,9 +204,11 @@ void Block(const char* label, const ImRect &bb, const ImVec4 &color, const float
 {
     ImDrawList* drawList = ImGui::GetWindowDrawList();
 
-    ImGui::ItemSize(bb.GetSize());
+    ImRect blockbb = bb;
+    blockbb.Min.y -= offset;
+    ImGui::ItemSize(blockbb.GetSize());
     const ImGuiID id = ImGui::GetID(label);
-    if (!ImGui::ItemAdd(bb, id))
+    if (!ImGui::ItemAdd(blockbb, id))
         return;
 
     { // Block background
