8adfc0012acf3377ee22ff46fdea1c7db9e6941b
HanaeRateau <hanaerateau@yahoo.fr>
2026-04-13 12:33:42 +0200

    Fixes excludes glyph ranges

--- SofaImGui/src/SofaImGui/ImGuiGUIEngine.cpp
+++ SofaImGui/src/SofaImGui/ImGuiGUIEngine.cpp
@@ -275,22 +275,24 @@ void ImGuiGUIEngine::initBackend(GLFWwindow* glfwWindow)
 
         ImGuiIO& io = ImGui::GetIO();
         io.Fonts->ClearFonts();
-        io.Fonts->AddFontFromMemoryCompressedTTF(OpenSans_Regular_compressed_data, OpenSans_Regular_compressed_size, 18 * yscale);
 
-        ImFontConfig config;
-        config.MergeMode = true;
-        config.GlyphMinAdvanceX = .0f; // Use if you want to make the icon monospaced
-        config.GlyphOffset.y = -2.f;
-        ImFontConfig configDejaVu(config);
-        configDejaVu.GlyphOffset.y = -5.f;
-        static const ImWchar icon_ranges[] = { ICON_MIN_FA, ICON_MAX_FA, 0 };
-        static const ImWchar icon_rangesDejaVu[] = { 0x2022, 0x2b54, 0};
+        ImFontConfig configOSR;
+        configOSR.GlyphOffset.y = -1.f;
+        ImFontConfig configFA;
+        configFA.MergeMode = true;
+        configFA.GlyphOffset.y = -2.f;
+        configFA.GlyphMinAdvanceX = .0f; // Use if you want to make the icon monospaced
+        ImFontConfig configDejaVu(configFA);
+        configDejaVu.GlyphOffset.y = -4.f;
+        static const ImWchar icon_rangesFA[] = { 0x0001, ICON_MIN_FA-1, ICON_MAX_FA+1, 0xfffe, 0 };
+        static const ImWchar icon_rangesDejaVu[] = { 0x0001, 0x2021, 0x2b55, 0xfffe, 0};
 
-        config.GlyphExcludeRanges = icon_rangesDejaVu;
-        configDejaVu.GlyphExcludeRanges = icon_ranges;
-        io.Fonts->AddFontFromMemoryCompressedTTF(FA_REGULAR_400_compressed_data, FA_REGULAR_400_compressed_size, 12 * yscale, &config, icon_ranges);
-        io.Fonts->AddFontFromMemoryCompressedTTF(FA_SOLID_900_compressed_data, FA_SOLID_900_compressed_size, 12 * yscale, &config, icon_ranges);
-        io.Fonts->AddFontFromMemoryCompressedTTF(DejaVuSans_compressed_data, DejaVuSans_compressed_size, 12 * yscale, &configDejaVu, icon_rangesDejaVu);
+        configFA.GlyphExcludeRanges = icon_rangesFA;
+        configDejaVu.GlyphExcludeRanges = icon_rangesDejaVu;
+        io.Fonts->AddFontFromMemoryCompressedTTF(OpenSans_Regular_compressed_data, OpenSans_Regular_compressed_size, 18 * yscale, &configOSR);
+        io.Fonts->AddFontFromMemoryCompressedTTF(FA_REGULAR_400_compressed_data, FA_REGULAR_400_compressed_size, 12 * yscale, &configFA);
+        io.Fonts->AddFontFromMemoryCompressedTTF(FA_SOLID_900_compressed_data, FA_SOLID_900_compressed_size, 12 * yscale, &configFA);
+        io.Fonts->AddFontFromMemoryCompressedTTF(DejaVuSans_compressed_data, DejaVuSans_compressed_size, 12 * yscale, &configDejaVu);
     }
 
     glGenBuffers(s_NB_PBOS, m_pbos);
