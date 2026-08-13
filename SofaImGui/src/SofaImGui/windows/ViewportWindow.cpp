a445d92067e8a3c04b7a49fc49098bedafcd8524
EulalieCoevoet <eulalie.coevoet@gmail.com>
2026-08-13 15:20:00 +0200

    minor cleaning

--- SofaImGui/src/SofaImGui/windows/ViewportWindow.cpp
+++ SofaImGui/src/SofaImGui/windows/ViewportWindow.cpp
@@ -629,7 +629,7 @@ void ViewportWindow::addSimulationTimeAndFPS()
                     ImGui::PushStyleColor(ImGuiCol_Text, COLOR_WHITE);
                     ImGui::Text("%.1f FPS", m_fps);
                     ImGui::PopStyleColor();
-                    ImGui::SetItemTooltip("FPS: frame per second \n Average %.2f ms per frame (%.1f FPS) \n GUI Average %.2f ms per frame (%.1f FPS)",
+                    ImGui::SetItemTooltip("FPS: Frame Per Second \n Average %.2f ms per frame (%.1f FPS) \n GUI Average %.2f ms per frame (%.1f FPS)",
                                           1000.0f / m_fps, m_fps, 1000.0f / io.Framerate, io.Framerate);
                 }
             }
